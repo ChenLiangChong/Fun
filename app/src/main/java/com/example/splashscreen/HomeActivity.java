1fe049634a150077a6c4290ca3a8802ffcdc15d6
Charlie <charlie19960414@gmail.com>
2020-10-13 00:10:33 +0800

    finish email login

---
+++ app/src/main/java/com/example/splashscreen/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView textUsername,textEmail;
+    Button btnLogout;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        textUsername = findViewById(R.id.textUsername);
+        textEmail = findViewById(R.id.textEmail);
+        btnLogout = findViewById(R.id.btnLogout);
+        mAuth=FirebaseAuth.getInstance();
+
+        if (mAuth!=null){
+            mUser = mAuth.getCurrentUser();
+            textEmail.setText(mUser.getEmail());
+            textUsername.setText(mUser.getDisplayName());
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(HomeActivity.this,SignUpActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/splashscreen/SignUpActivity.java
+++ app/src/main/java/com/example/splashscreen/SignUpActivity.java
@@ -1,8 +1,11 @@
 package com.example.splashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,14 +14,29 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.TypefaceSpan;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    Button btnFacebook,btnGoogle;
+    Button btnFacebook,btnGoogle,btnSignUp;
 
     private EditText inputEmail,inputPassword,inputUsername,inputBirth,inputGender;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog mLoadingBar;
+    FirebaseUser user;
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
@@ -47,7 +65,69 @@ public class SignUpActivity extends AppCompatActivity {
         ss2.setSpan(new TypefaceSpan(font2),13,19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         btnGoogle.setText(ss2);
 
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputUsername = findViewById(R.id.inputUsername);
+        inputBirth = findViewById(R.id.inputBirth);
+        inputGender = findViewById(R.id.inputGender);
+        btnSignUp = findViewById(R.id.btnSignUp);
 
+        mAuth = FirebaseAuth.getInstance();
+        mLoadingBar = new ProgressDialog(SignUpActivity.this);
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckValid();
+            }
+        });
+    }
+
+    private void CheckValid() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        final String username = inputUsername.getText().toString();
+
+        if(email.isEmpty()||!email.contains("@")){
+            showError(inputEmail,"Invalid Email");
+        }
+        else if(password.isEmpty()||password.length()<7){
+            showError(inputPassword,"Invalid Password");
+        }
+        else if(username.isEmpty()){
+            showError(inputUsername,"please enter username");
+        }
+        else{
+            mLoadingBar.setTitle("Sign Up");
+            mLoadingBar.setMessage("Please wait while check");
+            mLoadingBar.setCanceledOnTouchOutside(false);
+            mLoadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(SignUpActivity.this,"Successfully Sign Up",Toast.LENGTH_SHORT).show();
+                        user = mAuth.getCurrentUser();
+
+                        assert user != null;
+                        user.updateProfile(new UserProfileChangeRequest.Builder().setDisplayName("username").build());
+
+                        Intent intent = new Intent(SignUpActivity.this,HomeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(SignUpActivity.this, Objects.requireNonNull(task.getException()).toString(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
