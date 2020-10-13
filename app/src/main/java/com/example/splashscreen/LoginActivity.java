2b384bf3d8170320c7f5847718c27e7314c8aaf9
Charlie <charlie19960414@gmail.com>
2020-10-13 16:18:50 +0800

    各種修改

--- app/src/main/java/com/example/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/splashscreen/LoginActivity.java
@@ -2,7 +2,14 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
